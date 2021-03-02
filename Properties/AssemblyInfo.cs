8fe19e9886ed882d6ab892d98dbd7df4a3773147
Neil Ramrattan <neilramrattan@gmail.com>
2021-03-02 18:37:03 -0500

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RaceTrackTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RaceTrackTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3501fe69-d8f4-4b39-ba2d-3eae49df9a6a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RaceTrack.Models;
+using System;
+
+namespace RaceTrackTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Truck truck;
+        Car car;
+
+        [TestMethod]
+        public void WhenTruckHasNoStrapAndLiftedMoreThanFiveInchesInspectionFails()
+        {
+            //arrange
+            truck = new Truck
+            {
+                TowStrap = false,
+                LiftHeight = 6
+            };
+
+            //act
+            var allowed = truck.AllowOnTrack(0);
+
+            //assert
+            Assert.IsFalse(allowed);
+        }
+
+        [TestMethod]
+        public void WhenTruckHasNoStrapAndLiftedLessThanOrEqualToFiveInchesInspectionFails()
+        {
+            //arrange
+            truck = new Truck
+            {
+                TowStrap = false,
+                LiftHeight = 4
+            };
+
+            //act
+            var allowed = truck.AllowOnTrack(0);
+
+            //assert
+            Assert.IsFalse(allowed);
+        }
+
+        [TestMethod]
+        public void WhenTruckHasStrapAndLiftedLessThanOrEqualToFiveInchesInspectionPasses()
+        {
+            //arrange
+            truck = new Truck
+            {
+                TowStrap = true,
+                LiftHeight = 4
+            };
+
+            //act
+            var allowed = truck.AllowOnTrack(0);
+
+            //assert
+            Assert.IsTrue(allowed);
+        }
+
+        [TestMethod]
+        public void WhenTruckInspectionPassesButTrackCountReachedLimit()
+        {
+            //arrange
+            truck = new Truck
+            {
+                TowStrap = true,
+                LiftHeight = 4
+            };
+
+            //act
+            var allowed = truck.AllowOnTrack(5);
+
+            //assert
+            Assert.IsFalse(allowed);
+        }
+
+        [TestMethod]
+        public void WhenTruckHasStrapAndLiftedMoreThanFiveInchesInspectionFails()
+        {
+            //arrange
+            truck = new Truck
+            {
+                TowStrap = true,
+                LiftHeight = 6
+            };
+
+            //act
+            var allowed = truck.AllowOnTrack(0);
+
+            //assert
+            Assert.IsFalse(allowed);
+        }
+
+        [TestMethod]
+        public void WhenCarHasNoStrapAndTireWearMoreThanEightyFivePercentInspectionFails()
+        {
+            //arrange
+            car = new Car
+            {
+                TowStrap = false,
+                TireWear = 86
+            };
+
+            //act
+            var allowed = car.AllowOnTrack(0);
+
+            //assert
+            Assert.IsFalse(allowed);
+        }
+
+        [TestMethod]
+        public void WhenCarHasNoStrapAndTireWearLessThanEightyFivePercentInspectionFails()
+        {
+            //arrange
+            car = new Car
+            {
+                TowStrap = false,
+                TireWear = 84
+            };
+
+            //act
+            var allowed = car.AllowOnTrack(0);
+
+            //assert
+            Assert.IsFalse(allowed);
+        }
+
+        [TestMethod]
+        public void WhenCarHasStrapAndTireWearMoreThanEightyFivePercentInspectionFails()
+        {
+            //arrange
+            car = new Car
+            {
+                TowStrap = true,
+                TireWear = 86
+            };
+
+            //act
+            var allowed = car.AllowOnTrack(0);
+
+            //assert
+            Assert.IsFalse(allowed);
+        }
+
+        [TestMethod]
+        public void WhenCarHasStrapAndTireWearLessThanEightyFivePercentInspectionPasses()
+        {
+            //arrange
+            car = new Car
+            {
+                TowStrap = true,
+                TireWear = 84
+            };
+
+            //act
+            var allowed = car.AllowOnTrack(0);
+
+            //assert
+            Assert.IsTrue(allowed);
+        }
+
+        [TestMethod]
+        public void WhenCarInspectionPassesButTrackCountReachedLimit()
+        {
+            //arrange
+            car = new Car
+            {
+                TowStrap = true,
+                TireWear = 84
+            };
+
+            //act
+            var allowed = car.AllowOnTrack(5);
+
+            //assert
+            Assert.IsFalse(allowed);
+        }
+    }
+}
